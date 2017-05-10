b4b9932b3a0a064face4769275e8dd73edf37554
chewinggumk <chewinggumking@gmail.com>
2017-05-10 22:23:40 +0530

    Django Tutorial Part 8: User authentication and permissions

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        #MY APPS
     'catalog.apps.CatalogConfig',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR+ '/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
